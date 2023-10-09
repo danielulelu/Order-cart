f8e5f184293c086501f83c760cc237e8e3cec924
Eleven <danielulelu@gmail.com>
2023-10-09 06:25:42 +0300

    added custom colors and fonts

--- src/sections/details.tsx
+++ src/sections/details.tsx
@@ -63,7 +63,7 @@ const Details = () => {
             <Checkbox colorScheme="gray" defaultChecked>Ship to billing address.</Checkbox>
         </GridItem>
         <GridItem colSpan={2}>
-            <Button size="lg" w="full">Place Order</Button>
+            <Button colorScheme="brand" size="lg" w="full">Place Order</Button>
         </GridItem>
       </SimpleGrid>
     </VStack>
