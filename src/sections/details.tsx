ddded244423831153457f44149518f7ab0efed32
Eleven <danielulelu@gmail.com>
2023-10-09 06:08:25 +0300

    Implemented responsive design in Chakra-ui

--- src/sections/details.tsx
+++ src/sections/details.tsx
@@ -10,9 +10,11 @@ import {
   Select,
   Checkbox,
   Button,
+  useBreakpointValue
 } from "@chakra-ui/react";
 
 const Details = () => {
+  const colSpan = useBreakpointValue({ base: 2, md: 1});
   return (
     <VStack w="full" h="full" p={10} spacing={10} alignItems="flex-start">
       <VStack spacing={3} alignItems="flex-start">
@@ -20,13 +22,13 @@ const Details = () => {
         <Text>If you already have an account, click here!</Text>
       </VStack>
       <SimpleGrid columns={2} columnGap={3} rowGap={6} w="full">
-        <GridItem colSpan={1}>
+        <GridItem colSpan={colSpan}>
           <FormControl>
             <FormLabel>First Name</FormLabel>
             <Input placeholder="Daniel" />
           </FormControl>
         </GridItem>
-        <GridItem colSpan={1}>
+        <GridItem colSpan={colSpan}>
           <FormControl>
             <FormLabel>Last Name</FormLabel>
             <Input placeholder="Ulelu" />
@@ -38,13 +40,13 @@ const Details = () => {
             <Input placeholder="RSVP. Broken Dreams 2023" />
           </FormControl>
         </GridItem>
-        <GridItem colSpan={1}>
+        <GridItem colSpan={colSpan}>
           <FormControl>
             <FormLabel>City</FormLabel>
             <Input placeholder="Nairobi" />
           </FormControl>
         </GridItem>
-        <GridItem colSpan={1}>
+        <GridItem colSpan={colSpan}>
           <FormControl>
             <FormLabel>Country</FormLabel>
             <Select>
