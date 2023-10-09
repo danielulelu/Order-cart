3782132cd2ee085d9d3c8026f38201afa5753d9e
Eleven <danielulelu@gmail.com>
2023-10-09 06:38:34 +0300

    implemented withDefaultColorScheme and withDefaultVariant theme extensions

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -1,25 +1,40 @@
-import { extendTheme, theme as base } from "@chakra-ui/react";
+// import { withDefaultColorScheme } from '@chakra-ui/react';
+import {
+  extendTheme,
+  theme as base,
+  withDefaultVariant,
+  withDefaultColorScheme
+} from "@chakra-ui/react";
 
 const theme = extendTheme({
-    colors: {
-        brand: {
-            50: '#f5fee5',
-            100: '#e1fbb2',
-            200: '#cdf781',
-            300: '#b8ee56',
-            400: '#a2e032',
-            500: '#8ac919',
-            600: '#71ab09', 
-            700: '#578602',
-            800: '#3c5e00',
-            900: '#203300',
-        },
+  colors: {
+    brand: {
+      50: "#f5fee5",
+      100: "#e1fbb2",
+      200: "#cdf781",
+      300: "#b8ee56",
+      400: "#a2e032",
+      500: "#8ac919",
+      600: "#71ab09",
+      700: "#578602",
+      800: "#3c5e00",
+      900: "#203300",
     },
-    fonts: {
-        heading: `Montserat, ${base.fonts?.heading}`,
-        body: `Inter, ${base.fonts?.body}`
-    }
-});
+  },
+  fonts: {
+    heading: `Montserat, ${base.fonts?.heading}`,
+    body: `Inter, ${base.fonts?.body}`,
+  },
+},
+withDefaultVariant({
+    colorScheme: 'filled',
+    components: ['Input', 'Select'],
+}),
+withDefaultColorScheme({
+    colorScheme: 'brand',
+    components: ['Checkbox'],
+})
+
+);
 
 export default theme;
-
